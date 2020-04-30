3a32ca6254ed8ec33e139ba785c95ec0cc8b479e
Mauricio Leyzaola <mguerrero@repay.com>
2020-04-30 08:53:33 -0500

    #2 - completed scaning audio files

---
+++ src/server/pkg/files/scan_test.go
@@ -0,0 +1,106 @@
+package files
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/mauleyzaola/maupod/src/server/pkg/helpers"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestScanFiles(t *testing.T) {
+	var mode = os.ModePerm
+	// create file structure
+	uid, err := helpers.NewUUID()
+	require.NoError(t, err)
+	assert.NotEmpty(t, uid)
+
+	root := filepath.Join(os.TempDir(), uid)
+	err = os.MkdirAll(root, mode)
+	require.NoError(t, err)
+
+	defer func() {
+		err = os.RemoveAll(root)
+		assert.NoError(t, err)
+	}()
+
+	rush := filepath.Join(root, "rush")
+	err = os.MkdirAll(rush, mode)
+	require.NoError(t, err)
+
+	donna := filepath.Join(root, "donna")
+	err = os.MkdirAll(donna, mode)
+	require.NoError(t, err)
+
+	const (
+		tomSayer     = "Tom Sawyer.mp3"
+		subdivisions = "Subdivisions.flac"
+		lastDance    = "Last Dance.mp3"
+		springAffair = "Spring Affair.flac"
+	)
+
+	err = ioutil.WriteFile(filepath.Join(rush, tomSayer), nil, mode)
+	require.NoError(t, err)
+	err = ioutil.WriteFile(filepath.Join(rush, subdivisions), nil, mode)
+	require.NoError(t, err)
+	err = ioutil.WriteFile(filepath.Join(rush, "Rush in Rio.mp4"), nil, mode)
+	require.NoError(t, err)
+
+	err = ioutil.WriteFile(filepath.Join(donna, "The Collection.avi"), nil, mode)
+	require.NoError(t, err)
+	err = ioutil.WriteFile(filepath.Join(donna, lastDance), nil, mode)
+	require.NoError(t, err)
+	err = ioutil.WriteFile(filepath.Join(donna, springAffair), nil, mode)
+	require.NoError(t, err)
+
+	type args struct {
+		extensions  []string
+		directories []string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []string
+		wantErr bool
+	}{
+		{
+			name: "lookup mp3 files",
+			args: args{
+				extensions:  []string{".mp3"},
+				directories: []string{root},
+			},
+			wantErr: false,
+			want: []string{
+				filepath.Join(rush, tomSayer),
+				filepath.Join(donna, lastDance),
+			},
+		},
+		{
+			name: "lookup audio files with duplicates",
+			args: args{
+				extensions:  []string{".mp3", ".FLAC"},
+				directories: []string{root},
+			},
+			wantErr: false,
+			want: []string{
+				filepath.Join(rush, tomSayer),
+				filepath.Join(donna, lastDance),
+				filepath.Join(rush, subdivisions),
+				filepath.Join(donna, springAffair),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ScanFiles(tt.args.extensions, tt.args.directories...)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ScanFiles() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			assert.ElementsMatch(t, got, tt.want)
+		})
+	}
+}
--- src/server/pkg/files/walker_test.go
+++ src/server/pkg/files/walker_test.go
@@ -1,26 +1,74 @@
 package files
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/mauleyzaola/maupod/src/server/pkg/helpers"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
 
 func TestWalkFiles(t *testing.T) {
+	var mode = os.ModePerm
 	// create file structure
+	uid, err := helpers.NewUUID()
+	require.NoError(t, err)
+	assert.NotEmpty(t, uid)
 
-	type args struct {
-		root string
-		fn   WalkerFunc
+	root := filepath.Join(os.TempDir(), uid)
+	err = os.MkdirAll(root, mode)
+	require.NoError(t, err)
+
+	defer func() {
+		err = os.RemoveAll(root)
+		assert.NoError(t, err)
+	}()
+
+	beers := filepath.Join(root, "beers")
+	err = os.MkdirAll(beers, mode)
+	require.NoError(t, err)
+
+	wines := filepath.Join(root, "wines")
+	err = os.MkdirAll(wines, mode)
+	require.NoError(t, err)
+
+	err = ioutil.WriteFile(filepath.Join(beers, "leffe1.txt"), nil, mode)
+	require.NoError(t, err)
+	err = ioutil.WriteFile(filepath.Join(beers, "leffe2.txt"), nil, mode)
+	require.NoError(t, err)
+	err = ioutil.WriteFile(filepath.Join(beers, "leffe3.txt"), nil, mode)
+	require.NoError(t, err)
+
+	err = ioutil.WriteFile(filepath.Join(wines, "rioja1.txt"), nil, mode)
+	require.NoError(t, err)
+	err = ioutil.WriteFile(filepath.Join(wines, "rioja2.txt"), nil, mode)
+	require.NoError(t, err)
+	err = ioutil.WriteFile(filepath.Join(wines, "rioja3.txt"), nil, mode)
+	require.NoError(t, err)
+
+	type myfile struct {
+		base     string
+		fullpath string
+		isdir    bool
 	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		// TODO: Add test cases.
+
+	files := make(map[string]*myfile)
+
+	var fn WalkerFunc = func(name string, isDir bool) {
+		t.Logf("walking file: %s isDir: %v", name, isDir)
+		myfile := &myfile{
+			base:     filepath.Base(name),
+			fullpath: name,
+			isdir:    isDir,
+		}
+		_, ok := files[myfile.fullpath]
+		assert.False(t, ok, "we should not walk two times the same file")
+		files[myfile.fullpath] = myfile
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := WalkFiles(tt.args.root, tt.args.fn); (err != nil) != tt.wantErr {
-				t.Errorf("WalkFiles() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
+
+	err = WalkFiles(root, fn)
+	assert.NoError(t, err)
 }
