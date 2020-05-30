e28274bd326d8c94d6e2f042478aa78ce8a05a3e
mauleyzaola <mguerrero@repay.com>
2020-05-29 20:06:17 -0500

    wip

--- src/server/maupod/pkg/api/router.go
+++ src/server/maupod/pkg/api/router.go
@@ -21,6 +21,7 @@ func SetupRoutes(a *ApiServer, output io.Writer) http.Handler {
 
 	baseRouter.HandleFunc("/audio/scan", a.GlueHandler(a.AudioScanPost)).Methods(http.MethodOptions, http.MethodPost)
 	baseRouter.HandleFunc("/media/{field}/distinct", a.GlueHandler(a.PerformersGet)).Methods(http.MethodOptions, http.MethodGet)
+	baseRouter.HandleFunc("/media", a.GlueHandler(a.MediaListGet)).Methods(http.MethodOptions, http.MethodGet)
 
 	if output != nil {
 		return handlers.CombinedLoggingHandler(output, baseRouter)
