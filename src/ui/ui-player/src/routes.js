e05d1fcf5bcaab96537044586bdb187a33164034
mauleyzaola <mguerrero@repay.com>
2020-05-31 22:23:25 -0500

    added performer to the query string

--- src/ui/ui-player/src/routes.js
+++ src/ui/ui-player/src/routes.js
@@ -1,6 +1,6 @@
 import {objectToQueryString} from "./api";
 
-const linkAlbumList = media => `/media?${objectToQueryString({album:media.album} )}`;
+const linkAlbumList = media => `/media?${objectToQueryString({album:media.album, performer: media.performer} )}`;
 const linkGenreList = media => `/media?${objectToQueryString({genre:media.genre} )}`;
 const linkPerformerList = media => `/media?${objectToQueryString({performer:media.performer} )}`;
 
