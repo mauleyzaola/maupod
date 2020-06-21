2d1c2253ed186252b53a13f89a280597d9d98325
mauleyzaola <mguerrero@repay.com>
2020-06-20 21:02:00 -0500

    #56 missing files

--- src/server/cmd/player/main.go
+++ src/server/cmd/player/main.go
@@ -6,8 +6,6 @@ import (
 	"time"
 
 	"github.com/mauleyzaola/maupod/src/server/cmd/player/pkg"
-
-	"github.com/DexterLB/mpvipc"
 	"github.com/spf13/viper"
 )
 
@@ -29,102 +27,14 @@ func init() {
 }
 
 func run() error {
-	const songAdriana = "/media/mau/music-library/music/Adriana Calcanhotto/Maré/05 Mulher Sem Razão.m4a"
-	const songGino = "/media/mau/music-library/music/Gino Vannelli/Gino Vannelli Live/06 Hurts to Be In Love (Live).m4a"
-
-	process, err := pkg.MPVStart(songAdriana)
+	const songWarpigs = "/Users/mau/Downloads/music/Black Sabbath/1970 Paranoid (Black Box Remaster)/01 War Pigs , Luke's Wall.flac"
+	const songGrave = "/Users/mau/Downloads/music/Black Sabbath/1971 Master Of Reality (Black Box Remaster)/04 Children Of The Grave.flac"
+	wr, err := pkg.NewMPV(songWarpigs)
 	if err != nil {
 		return err
 	}
-	// need to have a startup time
-	time.Sleep(time.Second)
-
-	log.Println("pid: ", process.Pid)
-
-	conn := mpvipc.NewConnection("/tmp/mpv_socket")
-	err = conn.Open()
-	if err != nil {
-		return err
-	}
-	defer func() { _ = conn.Close() }()
-
-	var props = []string{}
-
-	for _, prop := range props {
-		value, err := conn.Get(prop)
-		if err != nil {
-			return err
-		}
-		log.Printf("prop: %s value: %v", prop, value)
-	}
-
-	play := func(p string) {
-		log.Println("loading file: ", p)
-		conn.Call("loadfile", p)
-		conn.Set("pause", false)
-	}
-
-	seek := func(offset int) {
-		log.Println("offset to: ", offset)
-		conn.Call("seek", offset, "exact")
-	}
-
-	sleep := func(secs int) {
-		log.Println("sleeping secs: ", secs)
-		time.Sleep(time.Second * time.Duration(secs))
-	}
-
-	play(songAdriana)
-	sleep(5)
-	seek(150)
-	sleep(5)
-	play(songGino)
-	sleep(2)
-	seek(200)
-	sleep(5)
-	seek(-75)
-	sleep(3)
-
-	log.Println("killing mpv process")
-	process.Kill()
-	return nil
-
-	//_, err = conn.Call("observe_property", 42, "volume")
-	//if err != nil {
-	//	fmt.Print(err)
-	//}
-
-	//ticker := time.NewTicker(time.Millisecond * 1000)
-	//go func() {
-	//	for {
-	//		select {
-	//		case t := <-ticker.C:
-	//			v, err := conn.Get("playback-time")
-	//			if err != nil {
-	//				panic(err)
-	//			}
-	//			fmt.Println("playback-time: ", v, t)
-	//		}
-	//	}
-	//}()
-
-	// TODO: implement state of paused/playing
-	// consider now only the default sound interface, but should support more in the future
-
-	//go func() {
-	//	conn.WaitUntilClosed()
-	//	stopListening <- struct{}{}
-	//}()
-
-	//for event := range events {
-	//	if event.ID == 42 {
-	//		log.Printf("volume now is %f", event.Data.(float64))
-	//	} else {
-	//		log.Printf("received event: %s id: %v", event.Name, event.Text)
-	//	}
-	//}
-
-	//log.Printf("mpv closed socket")
-
-	return nil
+	wr.Play(songWarpigs)
+	wr.PauseToggle()
+	time.Sleep(time.Second * 15)
+	return wr.Terminate()
 }
--- src/server/cmd/player/pkg/command.go
+++ src/server/cmd/player/pkg/command.go
@@ -2,13 +2,17 @@ package pkg
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 
 	"github.com/mauleyzaola/maupod/src/server/pkg/helpers"
 )
 
 const mpvProgram = "mpv"
+
+var socketFile = filepath.Join(os.TempDir(), "mpv_socket")
 
 // MPVStart forks a process passing the default parameters and plays a track
 func MPVStart(trackPath string) (*os.Process, error) {
@@ -19,7 +23,7 @@ func MPVStart(trackPath string) (*os.Process, error) {
 	// with these parameters mpv will need to issue a pause set to false to start playing
 	// giving the freedom of choice to load another file :D
 	// conn.Set("pause", false)
-	var pars = []string{"--no-video", "--input-unix-socket=/tmp/mpv_socket", "--quiet", "--pause", trackPath}
+	var pars = []string{"--no-video", fmt.Sprintf("--input-unix-socket=%s", socketFile), "--quiet", "--pause", trackPath}
 
 	cmd := exec.Command(mpvProgram, pars...)
 	if err := cmd.Start(); err != nil {
