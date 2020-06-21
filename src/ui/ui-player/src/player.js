893361c20a1e0029eea181b26cbab0b130b08df9
mauleyzaola <mauricio.leyzaola@gmail.com>
2020-06-21 16:34:56 -0500

    small hack to solve the missing info

--- src/ui/ui-player/src/player.js
+++ src/ui/ui-player/src/player.js
@@ -2,6 +2,11 @@ import {ipcCommand} from "./api";
 import {IPC_LOAD, IPC_PLAY} from "./consts";
 
 const playTrack = r => {
+    // TODO: fix this hack with the recorded_date being empty string
+    if(r.recorded_date === ''){
+        r.recorded_date = 0;
+    }
+    console.log(`requested to play file: ${r.location}`);
     const promises = [
         ipcCommand({
             command: IPC_LOAD,
