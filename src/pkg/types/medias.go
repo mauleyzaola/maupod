0b814bb5fa7dea689733e781c9feedc5f16fdf45
mauleyzaola <mauricio.leyzaola@gmail.com>
2020-07-05 17:37:37 -0500

    #83 handling media collections

--- src/pkg/types/medias.go
+++ src/pkg/types/medias.go
@@ -1,17 +1,44 @@
 package types
 
-import "github.com/mauleyzaola/maupod/src/pkg/pb"
+import (
+	"errors"
+
+	"github.com/mauleyzaola/maupod/src/pkg/pb"
+)
 
 type Medias []*pb.Media
 
-func (me Medias) InsertAt(m *pb.Media, index int) Medias {
-	var result Medias
-	if index > len(me) {
-		return me
+func (m Medias) InsertAt(media *pb.Media, index int) (Medias, error) {
+	var res Medias
+	if index < 0 || index > len(m) {
+		return nil, errors.New("index out of bounds")
 	}
-	first := me[0:index]
-	last := me[index:]
-	result = append(first, m)
-	result = append(result, last...)
-	return result
+	if len(m) == index {
+		return append(m, media), nil
+	}
+	res = append(m[:index+1], m[index:]...)
+	res[index] = media
+	return res, nil
+}
+
+func (m Medias) InsertTop(media *pb.Media) Medias {
+	res, _ := m.InsertAt(media, 0)
+	return res
+}
+
+func (m Medias) InsertBottom(media *pb.Media) Medias {
+	var index = len(m) - 1
+	if index == -1 {
+		index++
+	}
+	res, _ := m.InsertAt(media, index)
+	return res
+}
+
+func (m Medias) RemoveAt(index int) (Medias, error) {
+	if index < 0 || index > len(m)-1 {
+		return nil, errors.New("index out of bounds")
+	}
+
+	return append(m[:index], m[index+1:]...), nil
 }
--- src/pkg/types/medias_test.go
+++ src/pkg/types/medias_test.go
@@ -1,30 +1,244 @@
 package types
 
 import (
-	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 
 	"github.com/mauleyzaola/maupod/src/pkg/pb"
 )
 
+var (
+	m1 = &pb.Media{
+		Id: "1",
+	}
+	m2 = &pb.Media{
+		Id: "2",
+	}
+	m3 = &pb.Media{
+		Id: "3",
+	}
+)
+
 func TestMedias_InsertAt(t *testing.T) {
 	type args struct {
-		m     *pb.Media
+		media *pb.Media
 		index int
 	}
 	tests := []struct {
-		name string
-		me   Medias
-		args args
-		want Medias
+		name    string
+		m       Medias
+		args    args
+		want    Medias
+		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "index out of bounds",
+			args: args{
+				index: 1,
+			},
+			m:       nil,
+			wantErr: true,
+		},
+		{
+			name: "index out of bounds",
+			args: args{
+				index: -1,
+			},
+			m:       nil,
+			wantErr: true,
+		},
+		{
+			name: "add to empty slice",
+			args: args{
+				media: m1,
+				index: 0,
+			},
+			m:    nil,
+			want: Medias{m1},
+		},
+		{
+			name: "add to top of slice",
+			args: args{
+				media: m3,
+				index: 0,
+			},
+			m:    Medias{m1, m2},
+			want: Medias{m3, m1, m2},
+		},
+		{
+			name: "add to bottom of slice",
+			args: args{
+				media: m3,
+				index: 2,
+			},
+			m:    Medias{m1, m2},
+			want: Medias{m1, m2, m3},
+		},
+		{
+			name: "add to middle of slice",
+			args: args{
+				media: m2,
+				index: 1,
+			},
+			m:    Medias{m1, m3},
+			want: Medias{m1, m2, m3},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.me.InsertAt(tt.args.m, tt.args.index); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("InsertAt() = %v, want %v", got, tt.want)
+			got, err := tt.m.InsertAt(tt.args.media, tt.args.index)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.ElementsMatch(t, tt.want, got)
 			}
+		})
+	}
+}
+
+func TestMedias_RemoveAt(t *testing.T) {
+	type args struct {
+		index int
+	}
+	tests := []struct {
+		name    string
+		m       Medias
+		args    args
+		want    Medias
+		wantErr bool
+	}{
+		{
+			name: "index out of bounds",
+			args: args{
+				index: 1,
+			},
+			m:       nil,
+			wantErr: true,
+		},
+		{
+			name: "index out of bounds",
+			args: args{
+				index: -1,
+			},
+			m:       nil,
+			wantErr: true,
+		},
+		{
+			name: "remove from empty slice",
+			args: args{
+				index: 0,
+			},
+			m:       nil,
+			wantErr: true,
+		},
+		{
+			name: "remove from top of slice",
+			args: args{
+				index: 0,
+			},
+			m:    Medias{m3, m1, m2},
+			want: Medias{m1, m2},
+		},
+		{
+			name: "remove from bottom of slice",
+			args: args{
+				index: 2,
+			},
+			m:    Medias{m1, m2, m3},
+			want: Medias{m1, m2},
+		},
+		{
+			name: "remove from middle of slice",
+			args: args{
+				index: 1,
+			},
+			m:    Medias{m1, m2, m3},
+			want: Medias{m1, m3},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.m.RemoveAt(tt.args.index)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.ElementsMatch(t, tt.want, got)
+			}
+		})
+	}
+}
+
+func TestMedias_InsertTop(t *testing.T) {
+	type args struct {
+		media *pb.Media
+	}
+	tests := []struct {
+		name string
+		m    Medias
+		args args
+		want Medias
+	}{
+		{
+			name: "one item",
+			args: args{media: m1},
+			m:    nil,
+			want: Medias{m1},
+		},
+		{
+			name: "two items",
+			args: args{media: m1},
+			m:    Medias{m2},
+			want: Medias{m1, m2},
+		},
+		{
+			name: "three items",
+			args: args{media: m1},
+			m:    Medias{m2, m3},
+			want: Medias{m1, m2, m3},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.m.InsertTop(tt.args.media)
+			assert.ElementsMatch(t, tt.want, got)
+		})
+	}
+}
+
+func TestMedias_InsertBottom(t *testing.T) {
+	type args struct {
+		media *pb.Media
+	}
+	tests := []struct {
+		name string
+		m    Medias
+		args args
+		want Medias
+	}{
+		{
+			name: "one item",
+			args: args{media: m1},
+			m:    nil,
+			want: Medias{m1},
+		},
+		{
+			name: "two items",
+			args: args{media: m2},
+			m:    Medias{m1},
+			want: Medias{m1, m2},
+		},
+		{
+			name: "three items",
+			args: args{media: m3},
+			m:    Medias{m1, m2},
+			want: Medias{m1, m2, m3},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.m.InsertBottom(tt.args.media)
+			assert.ElementsMatch(t, tt.want, got)
 		})
 	}
 }
