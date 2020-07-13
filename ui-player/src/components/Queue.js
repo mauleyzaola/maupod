2884a3eaa1181fc9376590e4005fc72c00ae08e9
mauleyzaola <mauricio.leyzaola@gmail.com>
2020-07-12 20:14:41 -0500

    wip

---
+++ ui-player/src/components/Queue.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Queue extends React.Component{
+    render() {
+        return (
+            <div>
+                this is the queue component
+            </div>
+        )
+    }
+}
+
+export default Queue;
