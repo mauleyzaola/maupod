0422c7233be3257c9faf081dab50cc581bacdc9f
mauleyzaola <mauricio.leyzaola@gmail.com>
2020-08-29 21:03:54 -0500

    fixes #61 use sql count instead of mediainfo field

--- src/assets/db-migrations/20200604105241-view_albums.sql
+++ src/assets/db-migrations/20200604105241-view_albums.sql
@@ -6,7 +6,7 @@ create or replace view view_albums as
 select max(id) as id, album_identifier, max(album) as album, sum(cast(duration as int)) as duration,
        cast(avg(bit_rate) as bigint) as bit_rate, max(performer) as performer,
        max(genre) as genre, cast(max(recorded_date) as bigint) as recorded_date,
-       cast(avg(sampling_rate) as bigint) as sampling_rate, cast(max(track_name_total) as bigint) as track_name_total,
+       cast(avg(sampling_rate) as bigint) as sampling_rate, cast(count(*) as bigint) as track_name_total,
        max(image_location) as image_location, max(format) as format, cast(sum(file_size) as bigint) as file_size
 from media
 where album_identifier <> ''
