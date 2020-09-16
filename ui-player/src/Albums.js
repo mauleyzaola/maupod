47d621aba4fbbc35830d12cc4065dcbb33463e73
mauleyzaola <mguerrero@repay.com>
2020-09-16 14:49:04 -0500

    #147 smaller fonts and no rounded corners

--- ui-player/src/Albums.js
+++ ui-player/src/Albums.js
@@ -22,37 +22,45 @@ const AlbumCard = ({r}) => {
     return (
 
         <div className='album-card col-2 ml-1 card-deck'>
-            <div className="card border-secondary bg-dark p-0 mx-2 w-5 rounded-2">
+            <div className="card border-secondary bg-dark p-0 mx-2 w-5 no-rounded">
                 <div className='card-img-top'>      
                             <Thumbnail album={r} />
                 </div>       
                 <div class="card-body p-1 text bg-dark h-50">
-
                     <div className='row'>
-                        <div className='col mx-1 font-italic text-nowrap'>
+                        <div className='col mx-1 font-italic text-nowrap small'>
                         {r.recorded_date ? `${r.recorded_date}` : "unknown"}
                         </div>
-                        <div className='col font-italic text-nowrap'>
+                        <div className='col font-italic text-nowrap small'>
                         {r.track_name_total ? `Tracks: ${r.track_name_total}` : null}
                         </div>
                     </div>               
-                    <div className='mx-1 text-truncate'>
-                    
-                            <Link data-tip data-for="fullNameAlbum" to={linkAlbumView(r)}>
+                    <div className='mx-1 text-truncate small'>
+                            <Link data-tip data-for="fullNameAlbum" to={linkAlbumView(r)} title={r.album} >
                             {r.album}</Link>
-                            
                     </div>
-                    <div className='mx-1 text-muted '>
-                        <Link to={linkPerformerList(r)}>
+                    <div className='mx-1 text-muted small'>
+                        <Link to={linkPerformerList(r)} title={r.performer}>
                         {r.performer}
                         </Link>     
                     </div>
 
                 </div> 
                 <div class="card-footer">
-                <small className="text-muted h-5">
-                        {r.duration ? `Duration: ${msToString(r.duration)}` : null}
-                    </small>
+                    <div className="row">
+                        <div className="col">
+                            <small className="text-muted h-5">
+                                {r.duration ? msToString(r.duration) : null}
+                            </small>
+                        </div>
+                        <div className="col">
+                            <small className="text-muted h-5">
+                                <Link to={linkGenreList(r)} title={r.genre}>
+                                    {r.genre}
+                                </Link>
+                            </small>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>   
