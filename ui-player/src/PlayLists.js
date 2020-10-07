a9e2d2300dc5fb7389cbe057d03114a800883f39
Ismael Martinez <ismael.martinez.a@gmail.com>
2020-10-07 18:32:51 -0500

    fixes #194

--- ui-player/src/PlayLists.js
+++ ui-player/src/PlayLists.js
@@ -1,40 +1,107 @@
 import React, { Component } from "react";
 import API from "./api";
+import { Link } from "react-router-dom";
+// import PlayList from "./PlayListItem";
 
 const CardImagen = ({image_location}) => {
+    // console.log(image_location);
 
     if(!image_location) return null;
     return (
-        <img className="card-img-top p-2 w-30"
-            src={`${process.env.REACT_APP_MAUPOD_ARTWORK}/${image_location}`} 
-            alt="  "/>
+        <img className="card-img-top p-0 m-0 w-40"
+        src={`${process.env.REACT_APP_MAUPOD_ARTWORK}/${image_location}`} 
+        alt="  "/>
     )
 }
-const ListGroup = ({track}) => {
+const CardImagenPanel = ({image_location}) => {
+
+    if(!image_location) return null;
     return (
-        <a href="#" className="list-group-item list-group-item-action flex-column align-items-start bg-dark active px-2 py-0 mx-0">
-            <p className="my-0">{ track.track }</p>
-            <small className="py-0"> {track.performer} / {track.album}</small>
-        </a> 
+        <img className="card-img" style={{width:"99px", height:"99px"}}
+        src={`${process.env.REACT_APP_MAUPOD_ARTWORK}/${image_location}`} 
+        alt="  "/>
     )
 }
+
+const PanelImagen = ({tracks}) => {
+
+    return (
+        <>
+        <div className="row">
+            <div className="col pr-0 mr-0">
+                {<CardImagenPanel image_location={tracks[0]}/>}
+            </div>
+            <div className="col pl-0 ml-0">
+                {<CardImagenPanel image_location={tracks[1]}/>}
+            </div>
+        </div>
+        <div className="row">
+            <div className="col pr-0 mr-0">
+                {<CardImagenPanel image_location={tracks[2]}/>}
+            </div>    
+            <div className="col pl-0 ml-0">
+                {(tracks.count >= 3)? <CardImagenPanel image_location={tracks[3]}/>: ""}
+            </div>
+        </div>       
+        </>                        
+
+    )
+}
+
 
 const CardBody = ({playList}) => {
-    return (
-            <div  className="card border-secondary bg-dark p-2 pb-0 mx-2 no-rounded" style={{width:"220px"}} >
-                {<CardImagen image_location={ playList.imagen_location }/>}
-                <div className="card-body p-1 m-0">
-                    <h3 className="card-title text-center">{ playList.name }</h3>
-                </div>
 
-                <small>Include</small>
-                <div className="list-group p-0 mx-0">
-                    { playList.tracks.map(track => <ListGroup key={track.id} track={track} />) }
-                 </div>
-                <footer>Play</footer>
-        </div> 
+    const disctImage = [...new Set(playList.tracks.map(items => items.imagen_location))];    
+  
+    return(
+        <div  className="card border-dark bg-dark p-0 pb-0 mx-2 no-rounded" style={{width:"200px",height:"250px"}} >
+            <div className="card-body p-0 m-0">
+                    {(disctImage.length < 3)? 
+                    <CardImagen image_location={disctImage[0]}/>: 
+                    <PanelImagen tracks={disctImage}/>}
+                <div className="card-img-overlay text-white">
+                    <h5 className="card-title">
+                        <Link data-tip data-for="fullNameAlbum" to={playList.id} title="Play">{playList.name}</Link></h5>
+                    <p className="card-text">Last updated 3 mins ago</p>
+                </div>
+            </div>
+            <footer className="text-center font-weight-bold text-info">
+                <div className="row">
+                    
+                        <div className="col">
+                        <Link data-tip  to={playList.id} title="Play">{playList.tracks.length} tracks
+                        </Link> 
+                    </div>
+                </div>
+            </footer>
+     </div>
+
     )
 }
+// const ListGroup = ({track}) => {
+//     return (
+//         <a href="#" className="list-group-item list-group-item-action flex-column align-items-start bg-dark active px-2 py-0 mx-0">
+//             <p className="my-0">{ track.track }</p>
+//             <small className="py-0"> {track.performer} / {track.album}</small>
+//         </a> 
+//     )
+// }
+// const CardBody = ({playList}) => {
+//     return (
+//             <div  className="card border-secondary bg-dark p-2 pb-0 mx-2 no-rounded" style={{width:"220px"}} >
+//                 {<CardImagen image_location={ playList.imagen_location }/>}
+//                 <div className="card-body p-1 m-0">
+//                     <h3 className="card-title text-center">{ playList.name }</h3>
+//                 </div>
+
+//                 <small>Include</small>
+//                 <div className="list-group p-0 mx-0">
+//                     { playList.tracks.map(track => <ListGroup key={track.id} track={track} />) }
+//                  </div>
+//                 <footer>Play</footer>
+//         </div> 
+//     )
+// }
 
 class PlayLists extends Component{
 
@@ -59,6 +126,7 @@ class PlayLists extends Component{
                         track : "Intro / Stronger Than Me",
                         performer : "Amy Winehouse",
                         album  : "Frank",
+                        imagen_location : "4dd32f59-fbd0-48a3-b3c1-574dd3130353.png",
 
                      },
                      {
@@ -66,24 +134,27 @@ class PlayLists extends Component{
                         track : "You Sent Me Flying / Cherry",
                         performer : "Amy Winehouse",
                         album  : "Frank",
+                        imagen_location : "4dd32f59-fbd0-48a3-b3c1-574dd3130353.png",
                      },
                      {
                         id:"3", 
                         track : "Know You Now",
                         performer : "Amy Winehouse",
                         album  : "Frank",
+                        imagen_location : "4dd32f59-fbd0-48a3-b3c1-574dd3130353.png",
                      },
                      {
                         id:"4", 
                         track : "Fuck Me Pumps",
                         performer : "Amy Winehouse",
                         album  : "Frank",
+                        imagen_location : "4dd32f59-fbd0-48a3-b3c1-574dd3130353.png",
                      }                                          
               ]
             },
             {
               id: "7316c65a-4837-4244-87ea-c93c165e0163",
-              name: "Only Rock1",
+              name: "Musica Colombiana",
               imagen_location : "32612fe4-5b4a-42ff-824c-0ccb83d63eec.png",
               tracks: [
                 {
@@ -91,30 +162,34 @@ class PlayLists extends Component{
                    track : "Intro / Stronger Than Me",
                    performer : "Amy Winehouse",
                    album  : "Frank",
+                   imagen_location : "32612fe4-5b4a-42ff-824c-0ccb83d63eec.png",
                 },
                 {
                    id:"2", 
                    track : "You Sent Me Flying / Cherry",
                    performer : "Amy Winehouse",
                    album  : "Frank",
+                   imagen_location : "32612fe4-5b4a-42ff-824c-0ccb83d63eec.png",
                 },
                 {
                    id:"3", 
                    track : "Know You Now",
                    performer : "Amy Winehouse",
                    album  : "Frank",
+                   imagen_location : "4dd32f59-fbd0-48a3-b3c1-574dd3130353.png",
                 },
-                {
-                   id:"4", 
-                   track : "Fuck Me Pumps",
-                   performer : "Amy Winehouse",
-                   album  : "Frank",
-                }                                          
+                // {
+                //    id:"4", 
+                //    track : "Fuck Me Pumps",
+                //    performer : "Amy Winehouse",
+                //    album  : "Frank",
+                //    imagen_location : "4dd32f59-fbd0-48a3-b3c1-574dd3130353.png",
+                // }                                          
          ]              
             },
             {
               id: "2d2a5d55-5154-4679-aeb5-bcd772486617",
-              name: "Only Rock2",
+              name: "Rock Urbano",
               imagen_location: "3ad3b977-97d6-49bc-a88d-313fa496acba.png",
               tracks: [
                 {
@@ -122,24 +197,28 @@ class PlayLists extends Component{
                    track : "Intro / Stronger Than Me",
                    performer : "Amy Winehouse",
                    album  : "Frank",
+                   imagen_location : "4dd32f59-fbd0-48a3-b3c1-574dd3130353.png",
                 },
                 {
                    id:"2", 
                    track : "You Sent Me Flying / Cherry",
                    performer : "Amy Winehouse",
                    album  : "Frank",
+                   imagen_location: "3ad3b977-97d6-49bc-a88d-313fa496acba.png",
                 },
                 {
                    id:"3", 
                    track : "Know You Now",
                    performer : "Amy Winehouse",
                    album  : "Frank",
+                   imagen_location : "32612fe4-5b4a-42ff-824c-0ccb83d63eec.png",
                 },
                 {
                    id:"4", 
                    track : "Fuck Me Pumps",
                    performer : "Amy Winehouse",
                    album  : "Frank",
+                   imagen_location : "",
                 }                                          
          ]              
             }
@@ -154,13 +233,23 @@ class PlayLists extends Component{
     
 
     render(){
-
+        
         const {playLists} = this.state;
 
+        // playLists.map(playList => {
+        //     const distImage = [...new Set(playList.tracks.map(items => items.imagen_location))];    
+        //     console.log(distImage);
+        // })
+
+        // <div className="card-columns col-6">
+        // {playLists.map(playList => <CardBody key={playList.id} playList={ playList } />)}
+        
         return(
             <>
             <div className="card-columns col-6">
-                {playLists.map(playList => <CardBody key={playList.id} playList={ playList } />)}
+                 {/* {playLists.map(playList => <CardBody key={playList.id} playList={ playList } />)} */}
+                 
+                 {playLists.map(playList => <CardBody key={playList.id} playList={ playList } />)}
             </div>
             </>
         )
