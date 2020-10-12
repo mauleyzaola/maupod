253779c7f06e0115104327ec43b884fb9dc019b5
mauleyzaola <mauricio.leyzaola@gmail.com>
2020-10-12 10:02:17 -0500

    #194 fixed typos and naming stuff

--- ui-player/src/PlayLists.js
+++ ui-player/src/PlayLists.js
@@ -2,42 +2,42 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import API from "./api";
 
-const CardImagen = ({image_location}) => {
+const ImageCard = ({r}) => {
 
-    if(!image_location) return null;
+    if(!r.image_location) return null;
     return (
         <img className="card-img-top p-0 m-0 w-40"
-        src={`${process.env.REACT_APP_MAUPOD_ARTWORK}/${image_location}`} 
-        alt="  "/>
+        src={r.image_location}
+        alt="cover"/>
     )
 }
-const CardImagenPanel = ({image_location}) => {
+const ImageCardPanel = ({r}) => {
 
-    if(!image_location) return null;
+    if(!r.image_location) return null;
     return (
         <img className="card-img" style={{width:"99px", height:"99px"}}
-        src={`${process.env.REACT_APP_MAUPOD_ARTWORK}/${image_location}`} 
-        alt="  "/>
+        src={r.image_location}
+             alt="cover"/>
     )
 }
-const PanelImagen = ({tracks}) => {
+const ImagePanel = ({tracks}) => {
 
     return (
         <>
         <div className="row">
             <div className="col pr-0 mr-0">
-                {<CardImagenPanel image_location={tracks[0]}/>}
+                {<ImageCardPanel r={tracks[0]}/>}
             </div>
             <div className="col pl-0 ml-0">
-                {<CardImagenPanel image_location={tracks[1]}/>}
+                {<ImageCardPanel r={tracks[1]}/>}
             </div>
         </div>
         <div className="row">
             <div className="col pr-0 mr-0">
-                {<CardImagenPanel image_location={tracks[2]}/>}
+                {<ImageCardPanel r={tracks[2]}/>}
             </div>    
             <div className="col pl-0 ml-0">
-                {(tracks.count >= 3)? <CardImagenPanel image_location={tracks[3]}/>: ""}
+                {(tracks.count >= 3)? <ImageCardPanel r={tracks[3]}/>: ""}
             </div>
         </div>       
         </>                        
@@ -50,11 +50,14 @@ const CardBody = ({playList}) => {
         <div  className="card border-dark bg-dark p-0 pb-0 mx-2 no-rounded" style={{width:"200px",height:"250px"}} >
             <div className="card-body p-0 m-0">
                     {(playList.tracks.length < 3)? 
-                    <CardImagen image_location={playList.tracks[0]}/>: 
-                    <PanelImagen tracks={playList.tracks}/>}
+                    <ImageCard r={playList.tracks[0]}/>:
+                    <ImagePanel tracks={playList.tracks}/>}
                 <div className="card-img-overlay text-white">
                     <h5 className="card-title">
-                        <Link data-tip data-for="fullNameAlbum" to={playList.id} title="Play">{playList.name}</Link></h5>
+                        <Link data-tip data-for="fullNameAlbum" to={playList.id}
+                              title="Play">{playList.name}
+                        </Link>
+                    </h5>
                     <p className="card-text">Last updated 3 mins ago</p>
                 </div>
             </div>
@@ -76,30 +79,36 @@ class PlayLists extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            playLists:[],
+            playlists:[],
         }
     }
 
     loadData () {
-        let aPlayList = [];
-           API.playLists()
-              .then(data => {
-                data.map(items => { 
-                    API.playListItemsGet({id:items.id})
-                       .then(medias => {
-                            if (medias !== null) {
-                            const distImage = [...new Set(medias.map(item => item.media.image_location))];
-                                  items.tracks = distImage.filter(imagen => imagen !== undefined);
-                                  aPlayList.push(items);
-                        }
-                       })
-                }) 
-                this.setState({playLists: aPlayList});
-            })
+        const playlists = this.loadMockedData()
+        this.setState({playlists})
+        // this should be resolved in the server, not here
+        // TODO: create a gh issue with the parameters you need, so we return in one request, all the information
+
+        // let aPlayList = [];
+        //    API.playLists()
+        //       .then(data => {
+        //         data.map(items => {
+        //             API.playListItemsGet({id:items.id})
+        //                .then(medias => {
+        //                     if (medias !== null) {
+        //                     const distImage = [...new Set(medias.map(item => item.media.image_location))];
+        //                           items.tracks = distImage.filter(imagen => imagen !== undefined);
+        //                           aPlayList.push(items);
+        //                 }
+        //                })
+        //         })
+        //         this.setState({playLists: aPlayList});
+        //     })
             
     }
 
     loadMockedData() {
+        const sadeLoveDeluxeCoverURL = 'https://img.discogs.com/uRbsWHELT-KMowHGWp4WJPFdGM8=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-226306-1260746940.jpeg.jpg'
         return [
             {
               id: "9e196558-1bed-4b77-8dc8-867c87985fed",
@@ -110,7 +119,7 @@ class PlayLists extends Component{
                         track : "Intro / Stronger Than Me",
                         performer : "Amy Winehouse",
                         album  : "Frank",
-                        imagen_location : "4dd32f59-fbd0-48a3-b3c1-574dd3130353.png",
+                        image_location : sadeLoveDeluxeCoverURL,
 
                      },
                      {
@@ -118,21 +127,21 @@ class PlayLists extends Component{
                         track : "You Sent Me Flying / Cherry",
                         performer : "Amy Winehouse",
                         album  : "Frank",
-                        imagen_location : "4dd32f59-fbd0-48a3-b3c1-574dd3130353.png",
+                         image_location : sadeLoveDeluxeCoverURL,
                      },
                      {
                         id:"3", 
                         track : "Know You Now",
                         performer : "Amy Winehouse",
                         album  : "Frank",
-                        imagen_location : "4dd32f59-fbd0-48a3-b3c1-574dd3130353.png",
+                         image_location : sadeLoveDeluxeCoverURL,
                      },
                      {
                         id:"4", 
                         track : "Fuck Me Pumps",
                         performer : "Amy Winehouse",
                         album  : "Frank",
-                        imagen_location : "4dd32f59-fbd0-48a3-b3c1-574dd3130353.png",
+                         image_location : sadeLoveDeluxeCoverURL,
                      }                                          
               ]
             },
@@ -145,30 +154,23 @@ class PlayLists extends Component{
                    track : "Intro / Stronger Than Me",
                    performer : "Amy Winehouse",
                    album  : "Frank",
-                   imagen_location : "32612fe4-5b4a-42ff-824c-0ccb83d63eec.png",
+                    image_location : sadeLoveDeluxeCoverURL,
                 },
                 {
                    id:"2", 
                    track : "You Sent Me Flying / Cherry",
                    performer : "Amy Winehouse",
                    album  : "Frank",
-                   imagen_location : "32612fe4-5b4a-42ff-824c-0ccb83d63eec.png",
+                    image_location : sadeLoveDeluxeCoverURL,
                 },
                 {
                    id:"3", 
                    track : "Know You Now",
                    performer : "Amy Winehouse",
                    album  : "Frank",
-                   imagen_location : "4dd32f59-fbd0-48a3-b3c1-574dd3130353.png",
+                    image_location : sadeLoveDeluxeCoverURL,
                 },
-                // {
-                //    id:"4", 
-                //    track : "Fuck Me Pumps",
-                //    performer : "Amy Winehouse",
-                //    album  : "Frank",
-                //    imagen_location : "4dd32f59-fbd0-48a3-b3c1-574dd3130353.png",
-                // }                                          
-         ]              
+         ]
             },
             {
               id: "2d2a5d55-5154-4679-aeb5-bcd772486617",
@@ -179,28 +181,28 @@ class PlayLists extends Component{
                    track : "Intro / Stronger Than Me",
                    performer : "Amy Winehouse",
                    album  : "Frank",
-                   imagen_location : "4dd32f59-fbd0-48a3-b3c1-574dd3130353.png",
+                    image_location : sadeLoveDeluxeCoverURL,
                 },
                 {
                    id:"2", 
                    track : "You Sent Me Flying / Cherry",
                    performer : "Amy Winehouse",
                    album  : "Frank",
-                   imagen_location: "3ad3b977-97d6-49bc-a88d-313fa496acba.png",
+                    image_location : sadeLoveDeluxeCoverURL,
                 },
                 {
                    id:"3", 
                    track : "Know You Now",
                    performer : "Amy Winehouse",
                    album  : "Frank",
-                   imagen_location : "32612fe4-5b4a-42ff-824c-0ccb83d63eec.png",
+                    image_location : sadeLoveDeluxeCoverURL,
                 },
                 {
                    id:"4", 
                    track : "Fuck Me Pumps",
                    performer : "Amy Winehouse",
                    album  : "Frank",
-                   imagen_location : "",
+                   image_location : "",
                 }                                          
          ]              
             }
@@ -215,12 +217,12 @@ class PlayLists extends Component{
     
 
     render(){
-        const {playLists} = this.state;
+        const {playlists} = this.state;
         
         return(
             <>
             <div className="card-columns col-6">
-                 {playLists.map(playList => <CardBody key={playList.id} playList={ playList } />)}
+                 {playlists.map(p => <CardBody key={p.id} playList={ p } />)}
             </div>
             </>
         )
