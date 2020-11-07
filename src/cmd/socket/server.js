3915ecee932a598d1bf4618fabda6af00fde7d3e
mauleyzaola <mauricio.leyzaola@gmail.com>
2020-11-07 16:17:01 -0600

    #199 replaced js protobuf with constants

--- src/cmd/socket/server.js
+++ src/cmd/socket/server.js
@@ -4,8 +4,67 @@ const wss = new WebSocket.Server(wsOptions);
 const servers = ['nats://nats-server:4222']
 const NATS = require('nats');
 const nc = NATS.connect({json: true, servers: servers});
-const messages = require('./nodepb/messages_pb');
-const subjects = messages.Message;
+const messages = {
+    // ipc commands
+    IPC_PLAY : 0,
+    IPC_PAUSE : 1,
+    IPC_LOAD : 2,
+    IPC_VOLUME : 3,
+    IPC_SKIP : 4, // not really IPC but in the same workflow
+
+    MESSAGE_ARTWORK_SCAN : 10,
+    MESSAGE_AUDIO_SCAN : 11,
+    MESSAGE_MEDIA_INFO : 12,
+    MESSAGE_MEDIA_UPDATE_ARTWORK : 13,
+    MESSAGE_MEDIA_UPDATE_SHA : 14,
+    MESSAGE_MEDIA_DELETE : 15,
+    MESSAGE_TAG_UPDATE : 16,
+    MESSAGE_MEDIA_UPDATE : 17,
+    MESSAGE_MEDIA_SPECTRUM_GENERATE : 18,
+    MESSAGE_MEDIA_DB_SELECT : 19,
+    MESSAGE_MEDIA_EXTRACT_ARTWORK_FROM_FILE : 20,
+    MESSAGE_UPSERT_MEDIA_EVENT : 21,
+    MESSAGE_ARTWORK_FIND_ALBUM_COVER : 22,
+    MESSAGE_ARTWORK_DOWNLOAD : 23,
+    MESSAGE_SYNC_FILES : 24,
+    MESSAGE_SHA_SCAN : 25,
+
+    MESSAGE_IPC : 100,
+
+    // specific to mpv events
+    MESSAGE_MPV_EOF_REACHED : 202,
+    MESSAGE_MPV_PERCENT_POS : 203,
+    MESSAGE_MPV_TIME_POS : 204,
+    MESSAGE_MPV_TIME_REMAINING : 205,
+
+    // specific to player events
+    MESSAGE_EVENT_ON_TRACK_STARTED : 250,
+    MESSAGE_EVENT_ON_TRACK_FINISHED : 251,
+    MESSAGE_EVENT_ON_TRACK_PLAY_COUNT_INCREASE : 252,
+    MESSAGE_EVENT_ON_TRACK_SKIP_COUNT_INCREASE : 253,
+
+    // queue management
+    MESSAGE_QUEUE_LIST : 300,
+    MESSAGE_QUEUE_ADD : 301,
+    MESSAGE_QUEUE_REMOVE : 302,
+
+    // file management
+    MESSAGE_DIRECTORY_READ : 400,
+
+    // micro service discovery
+    MESSAGE_MICRO_SERVICE_ARTWORK : 420,
+    MESSAGE_MICRO_SERVICE_AUDIOSCAN : 421,
+    MESSAGE_MICRO_SERVICE_MEDIAINFO : 422,
+    MESSAGE_MICRO_SERVICE_PLAYER : 423,
+    MESSAGE_MICRO_SERVICE_RESTAPI : 424,
+    MESSAGE_MICRO_SERVICE_SOCKET : 425,
+
+    // real time socket events
+    MESSAGE_SOCKET_TRACK_POSITION_PERCENT : 500,
+    MESSAGE_SOCKET_TRACK_POSITION_PERCENT_CHANGE : 501,
+    MESSAGE_SOCKET_QUEUE_CHANGE : 502,
+    MESSAGE_SOCKET_PLAY_TRACK : 503,
+}
 
 // TODO: import from src/proto directory and generate the JS files there
 
@@ -22,7 +81,7 @@ const broadcastMessage = data => {
     }
 }
 
-nc.subscribe(subjects.MESSAGE_SOCKET_TRACK_POSITION_PERCENT, (msg) => {
+nc.subscribe(messages.MESSAGE_SOCKET_TRACK_POSITION_PERCENT, (msg) => {
     const { media, percent, seconds, secondsTotal } = msg;
     const data = {
         subject:'MESSAGE_SOCKET_TRACK_POSITION_PERCENT',
@@ -34,13 +93,13 @@ nc.subscribe(subjects.MESSAGE_SOCKET_TRACK_POSITION_PERCENT, (msg) => {
     broadcastMessage(data);
 })
 
-nc.subscribe(subjects.MESSAGE_SOCKET_QUEUE_CHANGE, (msg) => {
+nc.subscribe(messages.MESSAGE_SOCKET_QUEUE_CHANGE, (msg) => {
     broadcastMessage({
         subject: 'MESSAGE_SOCKET_QUEUE_CHANGE',
     });
 })
 
-nc.subscribe(subjects.MESSAGE_SOCKET_PLAY_TRACK, (msg) => {
+nc.subscribe(messages.MESSAGE_SOCKET_PLAY_TRACK, (msg) => {
     const { media } = msg;
     broadcastMessage({
         subject: 'MESSAGE_SOCKET_PLAY_TRACK',
@@ -64,7 +123,7 @@ wss.on('connection', ws => {
                         percent,
                         media,
                     }
-                    nc.publish(subjects.MESSAGE_SOCKET_TRACK_POSITION_PERCENT_CHANGE, payload);
+                    nc.publish(messages.MESSAGE_SOCKET_TRACK_POSITION_PERCENT_CHANGE, payload);
                     break;
                 default:
                     break;
