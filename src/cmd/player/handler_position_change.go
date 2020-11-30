be26d76af548c99708bbbf19e404779e4f05d993
mauleyzaola <mauricio.leyzaola@gmail.com>
2020-11-29 21:49:08 -0600

    fixed restart unmarshal message

--- src/cmd/player/handler_position_change.go
+++ src/cmd/player/handler_position_change.go
@@ -2,19 +2,25 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/mauleyzaola/maupod/src/protos"
 	"log"
+
+	"github.com/mauleyzaola/maupod/src/pkg/helpers"
+	"github.com/mauleyzaola/maupod/src/protos"
 
 	"github.com/nats-io/nats.go"
 )
 
 // handlerPositionPercentChange may come as JSON format
 func (m *MsgHandler) handlerPositionPercentChange(msg *nats.Msg) {
+	// TODO: fix this hack
+	// msg.Data can come as JSON from node or protobuf from handler
 	var input protos.SocketTrackPositionChangeInput
-	err := json.Unmarshal(msg.Data, &input)
+	err := helpers.ProtoUnmarshalJSON(msg.Data, &input)
 	if err != nil {
-		log.Println(err)
-		return
+		if err = json.Unmarshal(msg.Data, &input); err != nil {
+			log.Println(err)
+			return
+		}
 	}
 
 	if err = m.ipc.SeekAbsolute(input.Percent); err != nil {
